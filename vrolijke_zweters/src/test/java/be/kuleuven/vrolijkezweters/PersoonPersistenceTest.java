e0f680d1ecdc4d3f9c75b3783030079505ae7245
MaartenG-1849261 <maarten.gielkens@student.uhasselt.be>
2023-01-05 12:25:12 +0100

    Persoon persistence test gemaakt

---
+++ vrolijke_zweters/src/test/java/be/kuleuven/vrolijkezweters/PersoonPersistenceTest.java
@@ -0,0 +1,25 @@
+package be.kuleuven.vrolijkezweters;
+
+import be.kuleuven.vrolijkezweters.model.Persoon;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDate;
+
+public class PersoonPersistenceTest {
+
+    private EntityManager entityManager;
+
+    @BeforeEach
+    public void setUp() {
+        this.entityManager = EntityManagerProvider.getEntityManager();
+    }
+
+    @Test
+    public void persoonCanBePersisted() {
+        var testPersoon = new Persoon("Gielkens", "Maarten", LocalDate.of(2020, 1 , 8), "M", "maarten.gielkens@student.uhasselt.be", "12345", false);
+        entityManager.persist(testPersoon);
+    }
+}
